b87395cded76ae2258ee6f9bd6e54085dc0a8b63
jmotl96 <jmotl96@gmail.com>
2021-04-19 14:34:58 -0500

    Initial commit

---
+++ Lab5A/Lab5A/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab5A
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void NavigateButton_OnClicked_Grid(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Grid());
+        }
+
+        private async void NavigateButton_OnClicked_Absolute(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Absolute());
+        }
+
+        private async void NavigateButton_OnClicked_Clock(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Clock());
+        }
+    }
+}
